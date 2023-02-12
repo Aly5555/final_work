778f647bd4d610220415a89f83d7d796ef9fccd3
Albina Burtnik <allachka55555@gmail.com>
2023-02-12 15:07:31 +0300

    Сохранение4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
-﻿            string[] M;
-            Console.WriteLine("Введите массив строк через пробел");
-            string stroka = Console.ReadLine();
-            M = stroka.Split(' ');
-            var result = new string[M .Length];
+﻿            string[] array;
+            Console.WriteLine("Введите,пожалуйста, массив строк. Для разделения используйте пробел");
+            string str = Console.ReadLine();
+            array = str.Split(' ');
+            var resultArray = new string[array .Length];
             var realSize = 0;
+            foreach (var value in array )
+            {
+                if (value.Length <= 3)
+                {
+                    resultArray[realSize] = value;
+                    realSize++;
+                }
+            }
+             Console.WriteLine("Формирую массив из строк:");
+            Console.WriteLine(string.Join(Environment.NewLine, resultArray, 0, realSize));
+            Console.ReadKey(true);
