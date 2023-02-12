dca83335ca09f68ab61e86b13719b8b471f9c13b
Albina Burtnik <allachka55555@gmail.com>
2023-02-12 14:48:36 +0300

    Сохранение3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿            string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            M = stroka.Split(' ');
+            var result = new string[M .Length];
+            var realSize = 0;
